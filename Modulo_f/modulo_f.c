1597117396928a9f5424af84414f612f4aafe873
MiguelMartins <a93280@alunos.uminho.pt>
2020-12-06 16:39:03 +0000

    Tamanho dos ficheiros

--- Modulo_f/modulo_f.c
+++ Modulo_f/modulo_f.c
@@ -7,7 +7,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+long int tamanhoFicheiro (FILE *f){
+   if (!f) return 0;
+   else {
+      fseek(f, 0L, SEEK_END);
+      int tamanho = ftell(f); 
+      return tamanho;
+   }
+} //Inclui o caracter  ?? \r ??
 
 int main() {
     // Início da contagem do tempo de execução
@@ -16,7 +23,7 @@ int main() {
     // Abertura dos ficheiros
 
     FILE *orig;
-    orig = fopen("ficheiroExemplo.txt","r+"); // Ficheiro original
+    orig = fopen("aaa.txt","r+"); // Ficheiro original
 
     if (!orig) {
         printf("Erro ao abrir o ficheiro!"); // Caso haja erro na leitura do ficheiro original, o programa termina
@@ -25,7 +32,7 @@ int main() {
     // FILE *rle = fopen("ficheiroExemplo.txt.rle","w"); // Ficheiro rle
     // FILE *freq = fopen("ficheiroExemplo.txt.freq","w"); // Ficheiro freq
 
-
+    printf("O tamanho do ficheiro é %ld bytes\n", tamanhoFicheiro(orig));
 
 
     // Fechar os ficheiros
@@ -33,16 +40,20 @@ int main() {
     fclose(orig);
 
     // Fim da contagem do tempo de execução
-    clock_t fim = clock();
+//    clock_t fim = clock();
 
     // Impressão no terminal das informações sobre este módulo
-    printf("Tempo de execução: %f segundos", ((double)(fim - inicio)) / CLOCKS_PER_SEC);
-    return 0;
+ //   printf("Tempo de execução: %f segundos", ((double)(fim - inicio)) / CLOCKS_PER_SEC);
+//    return 0;
 }
 
 
-int leituraF(FILE *f) {
+/*int leituraF(FILE *f) {
     while (fread())
     fread();
     return ;
-}
+}    */
+
+
+
+   
