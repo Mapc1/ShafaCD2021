1e6fb58e6e697c0f44a37768a5e202d3f3c14c11
Jorge <jorge@antonio.lan>
2020-12-31 02:18:37 +0000

    troquei listas por arrays

--- mod_3/com_blocos_e_buffer.c
+++ mod_3/com_blocos_e_buffer.c
@@ -1,71 +1,41 @@
 //Criado dia 3 dezembro
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/*
-void imprime_bloco (int valoresLidos, char ** freq, char * fim) {
-	int carater_atual;
-    int ii;
-	for (carater_atual = 0; (freq[carater_atual]) != fim; carater_atual++) {
-        printf ("carater de imprime %d é: ", carater_atual);
-        if (freq[carater_atual] == NULL)  printf ("NADA \n");
-        else { 
-            printf("\n imprime else \n ");
-            char *bitsTalvez= freq[carater_atual];
-            for (ii = 0; bitsTalvez[ii] != '\0'; ii++) printf("%c  |  ", bitsTalvez[ii]); //Problema aqui!
-            printf("\n"); 
- 	    }
-    }
-}
-*/
-typedef struct dez6bits { 
-    int biti[8];
-    int bitiAtual;
-    struct dez6bits *prox;
-} *ListaBit;
+#include <time.h>
+
 //Converte [01110101101] num valor entre 0 e 257
-char converteByteEstupido (struct dez6bits isto)
-{
+char converteByteEstupido (char *byte) {
     char devolve = 0;
     int teste = 0;
-    int pos_atual=isto.bitiAtual ;
-    int dif;
-    dif = 8 - pos_atual;
-    //else  dif = 0;
-    pos_atual--;    
+    int pos_atual= 7 ; 
     int expoenteDois = 1;
-    while(pos_atual >= 0)
-    {
-        devolve += expoenteDois * (isto.biti[pos_atual]-48);
-        teste += expoenteDois * (isto.biti[pos_atual]-48);
+    while(pos_atual >= 0) {
+        devolve += expoenteDois * (byte[pos_atual]-48);
+        teste += expoenteDois * (byte[pos_atual]-48);
         expoenteDois *= 2;
         pos_atual--;
     }    
-    while (dif > 0) {devolve *=  2; dif--;}
     return devolve;
 }
-void converteListaArray (char* array_final, ListaBit list)
-{
+
+void converteListaArray (char* array_final, char *result) {
     //pos_atual no array_final
+    int i;
+    char byte [8];
+    int k;
+    int tam = strlen (result);
     int pos_atual = 0;
-    while (list != NULL)
-    {
-         array_final[pos_atual] = converteByteEstupido(*list);
+    for (i = 0; i< tam;) {
+        for (k = 0; k < 8 && i < tam ; k++, i++) byte [k] = result [i]; 
+        for (; k < 8; k++) byte[k] = '0'; 
+         array_final[pos_atual] = converteByteEstupido(byte);
          pos_atual++;
-         list = list->prox;
+         if (i == tam) return;
     }
 }
 
-//Cria lista vazia
-ListaBit initLista ()
-{
-    ListaBit novo = malloc(sizeof(struct dez6bits));
-    novo->bitiAtual = 0;
-    novo->prox = NULL;
-    return novo;
-}
 //Guarda os códigos a partir de um ficheiro ASCII
 char ** ler_bloco_ficheiro (char * buffer, int valoresLidos, char * fim, int * comp_cod_bloc) {
     //[Endereços das codificações de cada char]  a codificação do 'c' freq[0] 
@@ -73,38 +43,26 @@ char ** ler_bloco_ficheiro (char * buffer, int valoresLidos, char * fim, int * c
     int pos_atual_freq =0;  //posição atual no array das codificações
     int pos_atual_bufo = 0;
     int i;    
-    //for (i = 0; i < size; i++) freq[i] = -1;
     char c = buffer[pos_atual_bufo];
     //Depois alterar para ser só diferente de '@
     while((c == ';' || c == '0' || c == '1')  && pos_atual_freq < valoresLidos){ //64 = @ in ASCII
-        //printf ("\n %d \n ", pos_atual);
-   //     printf ("\n carater de leitura %d é: %c", pos_atual_freq, c);
         if (c != ';') {   // 59 == ;                   
             int comp_cod = pos_atual_bufo; //Guarda posição atual
             for (i = pos_atual_bufo; c == '0' || c == '1'; i++) c = buffer[i]; //Conta qual o comprimento do código 
             comp_cod = i - pos_atual_bufo; //Tamanho da codificação
-           // *comp_cod_bloc += comp_cod;
             freq[pos_atual_freq] = malloc( comp_cod * sizeof(char));
             char bitsTalvez[comp_cod];  //Depois tens de fazer free()
             //O array não precisa de ser char, porque só guarda 0's e 1's
             //Guarda no array bitsTalvez a codificação
-            //  printf(" \n Posição atual do array %d \n", pos_atual);
             c = buffer[pos_atual_bufo];
             for (i=0; c == '0' || c == '1' ; i++) { 
                 (bitsTalvez[i]) = c;
                 pos_atual_bufo++;
                 c = buffer[pos_atual_bufo];
             }
-            (bitsTalvez[i]) = '9';
-     //       printf (" -_- ");
-       //     for (i = 0; bitsTalvez[i] == '0' || bitsTalvez[i] == '1'; i++) printf("%c", bitsTalvez[i]); printf(" <- bits \n" );
+            (bitsTalvez[i]) = '\0';
             strcpy(freq[pos_atual_freq], bitsTalvez); 
-            // (freq[pos_atual]) = bitsTalvez;
-            //    c = fgetc(fp); //Ignorar o ; a seguir ao binário
             i = 9; //Apaga esta linha, é só para para no break
-
-
-
         }
         else (freq[pos_atual_freq] = NULL);
         pos_atual_bufo++;
@@ -115,109 +73,47 @@ char ** ler_bloco_ficheiro (char * buffer, int valoresLidos, char * fim, int * c
     *comp_cod_bloc += pos_atual_bufo+1;                     
     return freq;
 }
-///Acrescenta elementos À lista
-int addLista (ListaBit  cauda, char* bitsCod, int tam)
-{
-    int pos_atual;
-        int resposta = 0;
-        for (pos_atual = 0; cauda->bitiAtual < 8 && pos_atual < tam; pos_atual++, cauda->bitiAtual++)
-    
-        {
-            cauda->biti[cauda->bitiAtual] = bitsCod[pos_atual];
-            //cauda->bitiAtual++;
-        }
-    if (cauda->bitiAtual >= 8) {
-                        resposta++;             
-                        ListaBit novo = initLista();
-                           resposta+= addLista(novo, &bitsCod[pos_atual], tam - pos_atual);
-                                cauda->prox = novo; 
-                                  }
-    return resposta;
-}
 
 //buffer contêm ficheiro .txt
 //fptr .shaf, no fim para guardar o buffer
-void PontoShafa (char* buffer, FILE *fptr, char **freq, int tam_buffer)
-{
+
+char *nomeFicheiroExtensao(char *nomeFicheiro, char *extensao) {
+    size_t length = strlen(nomeFicheiro) + strlen(extensao) + 1;
+    char *concat = malloc(sizeof(char) * length);
+    if (!concat) return NULL; // Malloc error
+    snprintf(concat, length, "%s%s", nomeFicheiro, extensao);
+    return concat;
+}
+
+void PontoShafa (char* buffer, FILE *fptr, char **freq, int tam_buffer){
     int atual;
     char c;
-    //Criar endereço da última coisa da lista, para não ter de percorrer
-    ListaBit list = initLista();
-    ListaBit cauda = list;
+    char *result = malloc (256 * sizeof (char));
+    int size = 256;
     int resposta; 
-    int numListas = 1;
     tam_buffer--;
-    for (atual = 0; atual < tam_buffer; atual++)
-        {
-         c = buffer[atual];  //Pode dar erro <-, pode dar negativo?
-         char *bitsTalvez= freq[c]; //Juntar com a linha anterior
-        int tam;
-            for (tam = 0; bitsTalvez[tam] != '9'; tam++);
-        //Guarda a codificação do símbolo c
-        char codificacao[tam];
-        int i;
-        //Se for possível usar diretamente o *bitsTalvez em vez de codificação
-        for (i = 0; i < tam; i++) codificacao[i] = bitsTalvez[i]; 
-        resposta = 0;
-        resposta = addLista(cauda, codificacao, tam);
-        if (resposta) {numListas++; cauda = (cauda)->prox;}
+    char *new_arr = NULL;
+    printf ("AQUI\n");
+    for (atual = 0; atual < tam_buffer; atual++) {
+        if (atual > size-255) {
+            result = realloc (result, 2*size);
+            size *= 2;
         }
- /*      
-      list->biti[8]; 
-       list->biti[0] = 48;
-        list->biti[1] = 49;
-        list->biti[2] = 48;
-        list->biti[3] = 49;
-        list->biti[4] = 48;
-        list->biti[5] = 49;
-        list->biti[6] = 49;
-        list->bitiAtual = 7;
-        list->prox = NULL; */
-    char array_final[numListas];
-        fprintf(fptr, "@%d@",numListas );
-    converteListaArray(array_final, list);
-    fwrite(array_final , numListas , sizeof(char) , fptr );
+        result = nomeFicheiroExtensao (result, freq[buffer[atual]]);
+        //strcat (result, freq[buffer[atual]]);
+    }
+    printf ("AQUI\n");
+    printf ("%s\n", result);
+    int tam;
+    if (strlen (result) % 8 == 0) tam = strlen (result) / 8;
+    else tam = (strlen (result) / 8) + 1;
+    char array_final[atual];
+    fprintf(fptr, "@%d@", tam);
+    converteListaArray(array_final, result);
+    fwrite(array_final , tam, sizeof(char) , fptr );
     printf("\n AAA Olá %s \n", array_final);    
+
 }
-
-
-// Função que cria o .rle.shaf (ESTÁ SUPER INCOMPLETA!!)  (Falta ver pq é q o file .shaf n fica como o do sor e fazer para as repetições)
-void criaFIleRle(char **freq, char *buffer, char *fim, int tam) {
-    FILE *rle = fopen ("aaa.txt.rle.shaf", "wb"); //criar o file
-    int i = 0;
-    char zero = '0';
-    while (i < tam ) { // este 100 é apenas para teste, mudar depois
-             // Aparecem números negativos, não sei porquê
-            if (freq[buffer[i]] != NULL) {
-                                        printf(" %d \n", buffer[i] ); 
-                                        fwrite (freq[buffer[i]],1, 1, rle); 
-                                        }// para já ele só passa para binário os valores n repetidos mais de 4 vezes 
-            i++;    
-    
-}
-}
-//abcbdbcbabdbcbdbcbabacdbacdaabcbdd
-
-//Função que guarda num buffer o ficheiro .rle
-void guardaRLE (char **freq, char fim) {
-    int i = 0;
-    printf ("\n\n\n\n"); //apenas para separar dos outros testes
-    FILE *rle = fopen ("aaa.txt.rle", "rb+");
-    if ((rle == NULL)) printf("Error! opening file");
-    else {
-        fseek(rle, 0L, SEEK_END); //ir ao fim do ficheiro para ver o seu tamanho
-        int tam = ftell(rle);
-        fseek(rle, 0L, SEEK_SET); //voltar ao ínicio
-        char buffer[tam];
-        fgets (buffer, sizeof (buffer), rle); //já que o buffer é do tamanho do file, podemos pô-lo todo de uma vez (mt provavelmente vamos mudar depois)
-        printf ("%u %u %u", buffer[0],buffer[1], buffer[2]); //apenas um teste
-        fclose(rle); 
-        printf ("\n"); 
-        criaFIleRle(freq, buffer, &fim, tam);
-        }
-}
-
-
 
 //Se for o último bloco, isto podia ser diminuido
 char ** ler_PontoCod (FILE *fp, int valoresLidos, int * comp_cod_bloc) {    //Este fp é .cod
@@ -227,31 +123,26 @@ char ** ler_PontoCod (FILE *fp, int valoresLidos, int * comp_cod_bloc) {    //Es
     //Valor estúpido para controlar fim do array
     char fim = '?';
     char **freq = ler_bloco_ficheiro(buffer, valoresLidos, &fim, comp_cod_bloc);
-   // imprime_bloco(valoresLidos, freq, &fim);
-//    guardaRLE(freq, fim); //basta pôr isto em comentário para n testar a minha função
     return freq;
 }
 
 
-
-
-
 //Esta main abre o ficheiro e guarda um array com a frequência de cada bloco, 1 de cada vez
 int main () {
-	//char c[15];
+    clock_t inicio = clock();
 	FILE *fp, *fptr, *fpOrigi;
     int valoresLidos = 257;
     int tam_bloc;
     int *freq[valoresLidos];
     int i;
-    //char *c;
-	if ((fp = fopen("aaa.txt.rle.cod", "r+")) == NULL)  printf("Error! opening file");         // Program exits if file pointer returns NULL.
+	if ((fp = fopen("aaa.txt.cod", "r+")) == NULL)  printf("Error! opening file");         // Program exits if file pointer returns NULL.
     if ((fptr = fopen("aaaM.shaf","wb")) == NULL){
        printf("Error! opening file");
+       return 0;
     }
-
-    if ((fpOrigi = fopen("aaa.txt.rle","rb")) == NULL){
+    if ((fpOrigi = fopen("aaa.txt","r")) == NULL){
        printf("Error! opening file");
+       return 0;
     }
     else {
 		//Ler tipo de ficheiro
@@ -259,20 +150,14 @@ int main () {
 		fscanf(fp, "@%c", &tipo);
 		if (tipo == 'N') printf("Normal");
 		else printf("Reles");
-        //printf("@%c@", tipo);
 		//Número de blocos
 		int num_blocos;	
 		fscanf(fp, "@%d@", &num_blocos);
         printf("  número de blocos %d \n", num_blocos);
         //int num_bloc_temp = num_blocos;
-        
-  //      fwrite(&num_blocos, sizeof(int),1, fp); 
-//        fprintf(fptr, "OLA");  //NAO ESTA A DAR, NÃO ESCREVE NO FICHEIRO
         fprintf(fptr, "@%d", num_blocos);
-        
-            int comp_cod_bloc = ftell(fp);
+        int comp_cod_bloc = ftell(fp);
         while (num_blocos > 0) {
-           
             printf("\n Bloco atual (ao contrário): %d \n ", num_blocos);
             //informação para ler o ficheiro original
             fscanf(fp, "%d@", &tam_bloc);
@@ -280,10 +165,10 @@ int main () {
             //Count digits of tam_bloc
             int count = 0;
             int temp = tam_bloc;
-               while (temp != 0) {
-                        temp /= 10;     // n = n/10
-                        ++count;
-                    }
+            while (temp != 0) {
+                temp /= 10;     // n = n/10
+                ++count;
+            }
             comp_cod_bloc += count+1; 
             char ** freq = ler_PontoCod(fp, valoresLidos, &comp_cod_bloc);
             num_blocos--;
@@ -291,18 +176,15 @@ int main () {
             char buffer [tam_bloc+1];
             fread(&buffer, sizeof(char), tam_bloc, fpOrigi);
             buffer[tam_bloc+1] = '\0';
-            //printf("   \n     antes .shaf     \n");
             PontoShafa(buffer, fptr, freq, tam_bloc+1); 
-
             fseek(fp, comp_cod_bloc, SEEK_SET);
-        
-            }
+        }
     }
-    //leRLE (fp, valoresLidos);
     fclose(fp);
-
     fclose(fptr);
     fclose(fpOrigi);
+    clock_t fim = clock();
+    printf("Tempo de execução do módulo: %f milisegundos\n", ((double)(fim - inicio)) / CLOCKS_PER_SEC * 1000);
 	return 0;
 }
 
