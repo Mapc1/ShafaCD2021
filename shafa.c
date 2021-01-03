b84e96f1ae4f047999633e7d7bde17a1c31b461a
Mapc1 <marcocosta579@gmail.com>
2021-01-03 20:49:14 +0000

    Uncomment moduleFMain

--- shafa.c
+++ shafa.c
@@ -135,7 +135,7 @@ int main(int argc, char *argv[]){
 
   if(argc == 1) fprintf(stdout, HELP);
   opts = getOpts(argc, argv);
-//  if(opts->modF) moduleFMain(opts);
+  if(opts->modF) moduleFMain(opts);
   //if(modT)
   if(opts->modD) moduleDMain(opts);
   //if(modC)
